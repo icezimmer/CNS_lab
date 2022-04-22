6ba12dd0487dc7b5b34d7f50571f548476c64e75
Giuseppe Lombardi <g.lombardi11@studenti.unipi.it>
2022-04-22 22:33:18 +0200

    small fix

--- LAB2_2/Assignment1/main.m
+++ LAB2_2/Assignment1/main.m
@@ -4,7 +4,7 @@ input_patterns = [data.p0, data.p1, data.p2];
 memory_pattern = 0;
 distortion_prop = 0.05;
 
-num_config = 2;
+num_config = 10;
 for config=1:num_config
     bias = (1+1) * rand - 1;
     Model = Hopfield(input_patterns, memory_pattern+1, bias, distortion_prop);
@@ -20,4 +20,5 @@ for config=1:num_config
 end
 
 Model_best = Hopfield(input_patterns, memory_pattern+1, bias_best, distortion_prop);
+Model_best.video
 Model_best.plot
